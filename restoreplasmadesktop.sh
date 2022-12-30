730140565c73d1d84a62b2bf5609bb670fbf8bd0
JJenkx <31495916+JJenkx@users.noreply.github.com>
2022-12-30 06:27:05 -0500

    Update restoreplasmadesktop.sh

--- restoreplasmadesktop.sh
+++ restoreplasmadesktop.sh
@@ -1,30 +1,17 @@
 #!/usr/bin/env bash
 # Restore plasma desktop layout from backup
-if [ $UID -ne 0 ]; then
-echo "run with sudo"
-else
-sudo cp /home/jjenkx/.config/plasma-org.kde.plasma.desktop-appletsrc.bak /home/jjenkx/.config/plasma-org.kde.plasma.desktop-appletsrc
-
-sudo cp /home/jjenkx/.config/plasmashellrc.bak /home/jjenkx/.config/plasmashellrc
-
-sudo chmod 400 /home/jjenkx/.config/plasma-org.kde.plasma.desktop-appletsrc /home/jjenkx/.config/plasmashellrc
-
+sudo cp $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc.bak $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
+sudo cp $HOME/.config/plasmashellrc.bak $HOME/.config/plasmashellrc
+sudo chmod 400 $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/plasmashellrc
 timeout 5 kquitapp5 plasmashell
-
-pgrep -U $USER -x plasmashell &>/dev/null && pkill -U $USER -x plasmashell
-
-pgrep -U $USER -x plasmashell &>/dev/null && pkill -U $USER -x -9 plasmashell # here the process does not get to clean-up.
-
-killall -9 plasmashell #sends a signal to all processes running any of the specified commands
-
+pgrep -U $USER -x plasmashell &>/dev/null && sudo pkill -U $USER -x plasmashell
+sleep 1s
+pgrep -U $USER -x plasmashell &>/dev/null && sudo pkill -U $USER -x -9 plasmashell # here the process does not get to clean-up.
+sleep 1s
+sudo killall -9 plasmashell #sends a signal to all processes running any of the specified commands
+sleep 1s
 pgrep -U $USER -x plasmashell &>/dev/null && echo "ERROR: cannot kill plasmashell"
-
-sudo chmod 600 /home/jjenkx/.config/plasma-org.kde.plasma.desktop-appletsrc /home/jjenkx/.config/plasmashellrc
-
+sudo chmod 600 $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/plasmashellrc
 sleep 2s
-
 setsid >/dev/null 2>&1 </dev/null nohup kstart5 plasmashell 2>&1 >/dev/null & 
-
 echo "Script done"
-
-fi
